4a68af9923a328a70b3d30fda391d3380623b299
NicolleLouis <louisxnicolle@gmail.com>
2020-08-22 14:20:20 +0200

    Save sex for cortex import

--- data_storage/service/clean_data.py
+++ data_storage/service/clean_data.py
@@ -1,5 +1,16 @@
+from data_storage.enums.sex import Sex
+
+
 class CleanDataService:
     @staticmethod
     def clean_float_string(dirty_string):
         cleaned_float_string = dirty_string.replace(",", '.')
         return cleaned_float_string
+
+    @staticmethod
+    def detect_sex(sex_as_string):
+        if sex_as_string == 'M':
+            return Sex.Male.value
+        if sex_as_string == 'F':
+            return Sex.Female.value
+        return Sex.Unknown.value
--- data_storage/service/csv_reader_service.py
+++ data_storage/service/csv_reader_service.py
@@ -1,7 +1,6 @@
 import csv
 
 from data_storage.enums.brain_subzone import BrainSubZone
-from data_storage.enums.sex import Sex
 from data_storage.repositories.brain_quantification import BrainQuantificationRepository
 from data_storage.service.clean_data import CleanDataService
 
@@ -34,10 +33,11 @@ class CSVReaderService:
             if index_row == 0:
                 continue
             brain_name = csv_data[0][index_row]
+            sex = CleanDataService.detect_sex(csv_data[1][index_row])
             for sub_zone in BrainSubZone.get_all_non_empty_subzone():
-                ki_pos = csv_data[2 + sub_zone_order[sub_zone]][index_row]
-                ki_neg = csv_data[8 + sub_zone_order[sub_zone]][index_row]
-                area = csv_data[14 + sub_zone_order[sub_zone]][index_row]
+                ki_pos = csv_data[3 + sub_zone_order[sub_zone]][index_row]
+                ki_neg = csv_data[9 + sub_zone_order[sub_zone]][index_row]
+                area = csv_data[15 + sub_zone_order[sub_zone]][index_row]
                 BrainQuantificationRepository.save_brain_quantification(
                     ki_pos=ki_pos,
                     ki_neg=ki_neg,
@@ -46,16 +46,17 @@ class CSVReaderService:
                     sub_zone=sub_zone,
                     brain_name=brain_name,
                     slice_thickness=slice_thickness,
-                    stage=stage
+                    stage=stage,
+                    sex=sex,
                 )
             ki_pos_total = 0
-            for ki_pos in csv_data[2:7]:
+            for ki_pos in csv_data[3:8]:
                 ki_pos_total += int(ki_pos[index_row])
             ki_neg_total = 0
-            for ki_neg in csv_data[8:13]:
+            for ki_neg in csv_data[9:14]:
                 ki_neg_total += int(ki_neg[index_row])
             area_total = 0
-            for area in csv_data[14:19]:
+            for area in csv_data[15:20]:
                 area_total += float(CleanDataService.clean_float_string(area[index_row]))
             BrainQuantificationRepository.save_brain_quantification(
                 ki_pos=ki_pos_total,
@@ -65,7 +66,8 @@ class CSVReaderService:
                 sub_zone=BrainSubZone.Empty.value,
                 brain_name=brain_name,
                 slice_thickness=slice_thickness,
-                stage=stage
+                stage=stage,
+                sex=sex,
             )
 
     @staticmethod
@@ -80,7 +82,7 @@ class CSVReaderService:
             if index_row == 0:
                 continue
             brain_name = csv_data[0][index_row]
-            sex = CSVReaderService.detect_sex(csv_data[1][index_row])
+            sex = CleanDataService.detect_sex(csv_data[1][index_row])
             ki_pos = csv_data[2][index_row]
             ki_neg = csv_data[3][index_row]
             area = csv_data[4][index_row]
@@ -95,11 +97,3 @@ class CSVReaderService:
                 stage=stage,
                 sex=sex,
             )
-
-    @staticmethod
-    def detect_sex(sex_as_string):
-        if sex_as_string == 'M':
-            return Sex.Male.value
-        if sex_as_string == 'F':
-            return Sex.Female.value
-        return Sex.Unknown.value
